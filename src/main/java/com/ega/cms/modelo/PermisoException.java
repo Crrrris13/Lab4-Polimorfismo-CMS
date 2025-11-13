2b5c3e7c4f81d7c9f26245ed497ed3aac8abf02f
Sebastian García <estuardosebastian10@gmail.com>
2025-11-12 18:29:03 -0600

    Creacion de clase para excepciones personalizadas

--- src/main/java/com/ega/cms/modelo/PermisoException.java
+++ src/main/java/com/ega/cms/modelo/PermisoException.java
@@ -1,3 +1,10 @@
-public class PermisoException {
-    
+package com.ega.cms.modelo;
+
+// Descripción: Excepción personalizada para la programación defensiva
+// Se lanza cuando un usuario intenta realizar una acción sin los permisos adecuados
+
+public class PermisoException extends Exception {
+    public PermisoException(String mensaje) {
+        super(mensaje);
+    }
 }
